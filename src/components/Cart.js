4e309115aa2a8ae55db05d4eec963937d306e226
Jagdamba Tripathi <jagdtri2003@gmail.com>
2024-07-09 12:15:01 +0530

    added Promo Code Functionality

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,22 +1,24 @@
-import React, {useState,useContext} from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "../style/cart.css";
 import { Link } from "react-router-dom";
 import Header from "./Header";
 import { CartContext } from "../context/CartContex";
 
 const CartItem = ({ item, onRemove }) => (
-    
   <div className="cart-item">
-    <table >
+    <table>
       <tbody>
         <tr>
           <td>
-            <img src={item.image} className="cart-item-image" />
+            <img src={item.image} alt="" className="cart-item-image" />
           </td>
           <td>
             <h4>{item.name}</h4>
-            <p>Sku: {item.sku}<br/>
-            Brand: {item.brand}</p>
+            <p>
+              Sku: {item.sku}
+              <br />
+              Brand: {item.brand}
+            </p>
             <button className="remove-button" onClick={() => onRemove(item.id)}>
               Remove
             </button>
@@ -37,30 +39,22 @@ const CartItem = ({ item, onRemove }) => (
 );
 
 const Cart = () => {
-    const {cartItems,setCartItems} = useContext(CartContext);
-//   const [cartItems, setCartItems] = useState([
-//     {
-//       id: 1,
-//       name: "Nike air zoom pegasus 35",
-//       size: "L",
-//       color: "Green",
-//       price: 411.0,
-//       quantity: 1,
-//       image: "https://via.placeholder.com/300.png?text=Product", // replace with actual image URL
-//     },
-//     {
-//       id: 2,
-//       name: "Mix and match chuck taylor all star",
-//       size: "X",
-//       color: "Brown",
-//       price: 798.0,
-//       quantity: 2,
-//       image: "https://via.placeholder.com/300.png?text=Product", // replace with actual image URL
-//     },
-//   ]);
+  const { cartItems, setCartItems } = useContext(CartContext);
 
+  useEffect(() => {
+    setTotalAmount(
+      cartItems.reduce((total, item) => total + item.price * item.quantity, 0)
+    );
+  }, [cartItems]);
+
+  const [discount, setDiscount] = useState(null);
   const [promoCode, setPromoCode] = useState("");
-
+  const [totalAmount, setTotalAmount] = useState(() => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  });
   const handleRemove = (id) => {
     setCartItems(cartItems.filter((item) => item.id !== id));
   };
@@ -71,60 +65,79 @@ const Cart = () => {
 
   const handleApplyPromoCode = () => {
     // Apply promo code logic here
+    if (promoCode === "FREE20") {
+      setDiscount(totalAmount*0.2);
+    }
   };
-
-  const totalAmount = cartItems.reduce(
-    (total, item) => total + item.price * item.quantity,
-    0
-  );
-    // const totalAmount = 0;
-
 
   return (
     <>
-    <Header/>
-    <div className="cart-container">
-      <div className="breadcrumb">
-        <Link to="/">Home</Link> / <span>Shopping cart</span>
-      </div>
-      <div className="cart">
-        <div className="cart-header">
-            <table >
-                <thead>
-                    <tr>
+      <Header />
+      <div className="cart-container">
+        <div className="breadcrumb">
+          <Link to="/">Home</Link> / <span>Shopping cart</span>
+        </div>
+        {cartItems.length === 0 ? (
+          <div className="empty-cart">
+            <h2>Shopping cart</h2>
+            <p>Your cart is empty!</p>
+            <Link to="/" className="continue-shopping-button">
+              CONTINUE SHOPPING &nbsp;
+              <i className="fa-light fa-arrow-right"></i>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <div className="cart">
+              <div className="cart-items-container">
+                <div className="cart-header">
+                  <table className="table-header">
+                    <thead>
+                      <tr>
                         <td colSpan={3}>Product</td>
                         <td>Price</td>
                         <td>Quantity</td>
                         <td>Total</td>
-                    </tr>
-                </thead>
-            </table>
-        </div>
-        <div className="cart-items">
-          {cartItems.map((item) => (
-            <CartItem key={item.id} item={item} onRemove={handleRemove} />
-          ))}
-        </div>
-        <div className="order-summary">
-          <h2>Order summary</h2>
-          <p>Sub total: ₹ {totalAmount.toFixed(2)}</p>
-          <p>Total: ₹ {totalAmount.toFixed(2)}</p>
-          <p>(Inclusive of tax ₹ 0.00)</p>
-          <button className="checkout-button">CHECKOUT</button>
-        </div>
-        <div className="promo-code">
-          <h3>Promotion code?</h3>
-          <input
-            type="text"
-            value={promoCode}
-            onChange={handlePromoCodeChange}
-            placeholder="Enter coupon code"
-          />
-          <button className="apply-button" onClick={handleApplyPromoCode}>
-            Apply
-          </button>
-        </div>
-      </div>
+                      </tr>
+                    </thead>
+                  </table>
+                </div>
+                <div className="cart-items">
+                  {cartItems.map((item) => (
+                    <CartItem
+                      key={item.id}
+                      item={item}
+                      onRemove={handleRemove}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="order-summary-container">
+                <div className="order-summary">
+                  <h2>Order summary</h2>
+                  <p>Sub total: ₹ {totalAmount.toFixed(2)}</p>
+                  {discount && <p>Discount: ₹ {discount.toFixed(2)} ({promoCode})</p> }
+                  <p>Total: ₹ {discount ? totalAmount.toFixed(2)-discount.toFixed(2) : totalAmount.toFixed(2)}</p> 
+                  <p>(Inclusive of tax ₹ 0.00)</p>
+                  <button className="checkout-button">CHECKOUT</button>
+                </div>
+              </div>
+            </div>
+            <div className="promo-code">
+              <h3>Promotion code?</h3>
+              <br/>
+              <input
+                type="text"
+                value={promoCode}
+                onChange={handlePromoCodeChange}
+                placeholder="Enter coupon code"
+              />
+              <button className="apply-button" onClick={handleApplyPromoCode}>
+                Apply
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
