e8986acf4d74deb3ec93bdff64533f213e4bc4dd
Jagdamba Tripathi <jagdtri2003@gmail.com>
2024-07-11 15:33:02 +0530

    fixed scrolling issue

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -1,67 +1,90 @@
-import React,{useState,useContext} from 'react';
-import '../style/itemcomponent.css';
-import { useParams,Link } from 'react-router-dom';
-import Header from './Header';
-import { CartContext } from '../context/CartContex.js';
-import {successToast} from './ToastComponent.js'
-import product from '../products/products.js';
-import Footer from './Footer.js';
-import StarRatings from 'react-star-ratings';
+import React, { useState,useEffect, useContext } from "react";
+import "../style/itemcomponent.css";
+import { useParams, Link } from "react-router-dom";
+import Header from "./Header";
+import { CartContext } from "../context/CartContex.js";
+import { successToast } from "./ToastComponent.js";
+import product from "../products/products.js";
+import Footer from "./Footer.js";
+import StarRatings from "react-star-ratings";
 
 const ItemComponent = () => {
-    const { id } = useParams();
-    const [count, setCount] = useState(1);
-    const {cartItems,addToCart} = useContext(CartContext);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    const item = product[id];
+  const { id } = useParams();
+  const [count, setCount] = useState(1);
+  const { cartItems, addToCart } = useContext(CartContext);
 
-    const handleAddToCart = (e) => {
-        e.target.setAttribute('disabled','true');
-        addToCart(id,{id,name:item.name,quantity:count,sku:`Sku-${id}`,brand:'Brand 1',price:item.price,image:item.image},count);
-        successToast("Item Added to Cart !!");
-        setTimeout(()=>{
-          e.target.removeAttribute('disabled');
-        },5000);
-    }
-    console.log(cartItems);
+  const item = product[id];
+
+  const handleAddToCart = (e) => {
+    e.target.setAttribute("disabled", "true");
+    addToCart(
+      id,
+      {
+        id,
+        name: item.name,
+        quantity: count,
+        sku: `Sku-${id}`,
+        brand: item.brand,
+        price: item.price,
+        image: item.image,
+      },
+      count
+    );
+    successToast("Item Added to Cart !!");
+    setTimeout(() => {
+      e.target.removeAttribute("disabled");
+    }, 5000);
+  };
+  console.log(cartItems);
   return (
     <>
-    <Header/>
-    <div className="breadcrumb">
-          <Link to="/">Home</Link> / <span>{item.name}</span>
-    </div>
-    <div style={{margin:'30px'}} className="item-container">
-      <div className="item-image">
-        <img style={{marginTop:'30px'}} src={item.image} alt={item.name} />
-        <br/>
-        <StarRatings rating={4+Math.random()}         
-        starRatedColor="orange"
-        starDimension="40px"
-        starSpacing="5px"
-        />
+      <Header />
+      <div className="breadcrumb">
+        <Link to="/">Home</Link> / <span>{item.name}</span>
       </div>
-      <div className="item-details">
-        <h2 className="item-name">{item.name}</h2>
-        <p><strong>SKU ID:</strong>  Sku-{id}</p>
-        <p>
-            <strong>Brand:</strong> Brand 1
-        </p>
-        <div className="item-price">
-          <span className="price">₹ {item.price}</span>
-          {item.discountPrice && (
-            <span className="discount-price">₹ {item.discountPrice}</span>
-          )}
+      <div style={{ margin: "30px" }} className="item-container">
+        <div className="item-image">
+          <img style={{ marginTop: "30px" }} src={item.image} alt={item.name} />
+          <br />
+          <StarRatings
+            rating={4 + Math.random()}
+            starRatedColor="orange"
+            starDimension="40px"
+            starSpacing="5px"
+          />
         </div>
-        <div className='count-btn'>
-            <button onClick={()=>count > 1 ? setCount(count-1) : null }>-</button>
-            <input id='count' value={count} type='number' min={1} readOnly/>
-            <button onClick={()=>setCount(count+1)}>+</button>
+        <div className="item-details">
+          <h2 className="item-name">{item.name}</h2>
+          <p>
+            <strong>SKU ID:</strong> Sku-{id}
+          </p>
+          <p>
+            <strong>Brand:</strong> {item.brand}
+          </p>
+          <div className="item-price">
+            <span className="price">₹ {item.price}</span>
+            {item.discountPrice && (
+              <span className="discount-price">₹ {item.discountPrice}</span>
+            )}
+          </div>
+          <div className="count-btn">
+            <button onClick={() => (count > 1 ? setCount(count - 1) : null)}>
+              -
+            </button>
+            <input id="count" value={count} type="number" min={1} readOnly />
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+          <button onClick={handleAddToCart} className="add-to-cart">
+            Add to Cart
+          </button>
+          <p className="item-specifications">{item.specifications}</p>
         </div>
-        <button onClick={handleAddToCart} className="add-to-cart">Add to Cart</button>
-        <p className="item-specifications">{item.specifications}</p>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
--- src/products/products.js
+++ src/products/products.js
@@ -4,42 +4,48 @@ const product = {
         name:'Blue Sapphire Pendant',
         price:54799,
         discountPrice:59999,
-        specifications:'A timeless and elegant Blue Sapphire Pendant that exudes sophistication and luxury. Perfect for special occasions or as a statement piece for everyday wear.'
+        specifications:'A timeless and elegant Blue Sapphire Pendant that exudes sophistication and luxury. Perfect for special occasions or as a statement piece for everyday wear.',
+        brand:'Blue Stone'
     },
     1002:{
         image:'https://kinclimg3.bluestone.com/f_jpg,c_scale,w_828,q_80,b_rgb:f0f0f0/giproduct/BIDG0319R180_YAA18DIG6XXXXXXXX_ABCD00-PICS-00001-1024-66194.png',
         name:'The Gianna Ring',
         price:54790,
         discountPrice:56364,
-        specifications:'An exquisite Diamond Ring crafted in 18Kt Yellow Gold, featuring brilliant diamonds that add a touch of elegance and sophistication. Perfect for engagements, anniversaries, or as a luxurious gift.'
+        specifications:'An exquisite Diamond Ring crafted in 18Kt Yellow Gold, featuring brilliant diamonds that add a touch of elegance and sophistication. Perfect for engagements, anniversaries, or as a luxurious gift.',
+        brand:'Blue Stone'
     },
     1003:{
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKqaVlTzAjkWzG8_QJFqQRevGRCBcOefiPLw&s',
         name:'TISSOT Sport Analog Watch',
         price:43499,
         discountPrice:49999,
-        specifications:"Amaze everyone around you by wearing this men's analog watch from TISSOT. The blue round dial is protected by a sapphire glass. It features plain 3-hand and a crown for time adjustment. The stainless steel strap comes in a silver hue that further elevates the look of the timekeeper. Besides, it is completed with a fold-over push button clasp with safety clasp to ensure a secure fit on the wrist."
+        specifications:"Amaze everyone around you by wearing this men's analog watch from TISSOT. The blue round dial is protected by a sapphire glass. It features plain 3-hand and a crown for time adjustment. The stainless steel strap comes in a silver hue that further elevates the look of the timekeeper. Besides, it is completed with a fold-over push button clasp with safety clasp to ensure a secure fit on the wrist.",
+        brand:'TISSOT'
     },
     1004:{
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgW8FHvRuH6AlUkCe481pnxZeQq4DAWHMdmA&s',
         name:'Air Tube Earphones Headset',
         price:1499,
         discountPrice:1999,
-        specifications:'The Air Tube Earphones Headset is a high-quality earphone that offers superior sound'
+        specifications:'The Air Tube Earphones Headset is a high-quality earphone that offers superior sound',
+        brand:'Air Tube'
     },
     1005:{
         image:'https://cdn.thewirecutter.com/wp-content/media/2023/10/smartphone-2048px-4861.jpg?auto=webp&quality=10&width=1024',
         name:'Samsung Galaxy S21 Ultra 5G',
         price:22379,
         discountPrice:25802,
-        specifications:'The Samsung Galaxy S21 Ultra 5G is a high-end smartphone that offers a powerful processor, a large display, and a long-lasting battery. It also has a triple rear camera setup that allows you to take stunning photos and videos.'
+        specifications:'The Samsung Galaxy S21 Ultra 5G is a high-end smartphone that offers a powerful processor, a large display, and a long-lasting battery. It also has a triple rear camera setup that allows you to take stunning photos and videos.',
+        brand:'Samsung'
     },
     1006:{
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn2yscgkxDOnZgwVJQt6Pa8VGrfBfJe5D5pA&s',
         name:'LG Inverter AC, 2 Ton',
         price: 49599,
         discountPrice: 55999,
-        specifications:'The LG Inverter AC is a high-efficiency air conditioner that uses an inverter technology to maintain a consistent temperature in your room. It also has a self-diagnosis feature that helps you to identify and fix any issues with the AC.'
+        specifications:'The LG Inverter AC is a high-efficiency air conditioner that uses an inverter technology to maintain a consistent temperature in your room. It also has a self-diagnosis feature that helps you to identify and fix any issues with the AC.',
+        brand:'LG'
     }
 }
 
