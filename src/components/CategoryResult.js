5cf0d5816fb26501c52256051181d199e338af03
Jagdamba Tripathi <jagdtri2003@gmail.com>
2024-07-13 21:12:29 +0530

    added sor by in categories

--- src/components/CategoryResult.js
+++ src/components/CategoryResult.js
@@ -1,8 +1,9 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import { useParams,Link } from 'react-router-dom';
 import product from '../products/products';
 import ProductCard from './ProductCard';
 import Header from './Header';
+import Footer from './Footer';
 
 function CategoryResult() {
 
@@ -10,23 +11,52 @@ function CategoryResult() {
       window.scrollTo(0, 0);
     },[])
 
+    const [sortCriteria, setSortCriteria] = useState('none');
+    const [sortedProducts, setSortedProducts] = useState([]);
+
     const { categoryName } = useParams();
-    const products = Object.values(product).filter((product)=> product.category.toLowerCase() === categoryName.toLowerCase());
-    console.log(products);
+
+    useEffect(() => { 
+
+      const products = Object.values(product).filter((product)=> product.category.toLowerCase() === categoryName.toLowerCase());
+
+      if (sortCriteria === 'Price') {
+        products.sort((a, b) => a.price - b.price);
+      } else if (sortCriteria === 'Name') {
+        products.sort((a, b) => a.name.localeCompare(b.name));
+      }
+      setSortedProducts(products);
+    }, [categoryName, sortCriteria]);
+
+    const handleSortChange = (e) => {
+      setSortCriteria(e.target.value);
+    };
+
+
   return (
     <>
     <Header/>
     <div className="breadcrumb">
         <Link to="/">Home</Link> / <span>{categoryName[0].toUpperCase()+categoryName.slice(1)}</span>
     </div>
-    <div className="category">
+    <div className="category" style={{maxWidth:'90vw'}}>
+      <div className='category-header'>
       <h2>Showing Results for {categoryName[0].toUpperCase()+categoryName.slice(1)} ...</h2>
+      <div>Sort by &nbsp;
+      <select  value={sortCriteria} onChange={handleSortChange}>
+        <option selected>None</option>
+        <option>Price: Low to High</option>
+        <option>Price: High to Low</option>
+        <option>Name</option>
+      </select></div>
+      </div>
       <div style={{paddingBottom:'30px'}} className="product-list">
-        {products.map((product) => (
+        {sortedProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </div>
+    <Footer/>
     </>
   )
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React,{useContext,useEffect,useState} from 'react';
+import { CartContext } from '../context/CartContex';
 import { Link } from 'react-router-dom';
 import '../style/productcard.css';
 import StarRatings from 'react-star-ratings';
+import { successToast } from './ToastComponent';
 
 function ProductCard({product}) {
+
+  const {addToCart} = useContext(CartContext);
+  const [starRating, setStarRating] = useState(4);
+  const [limitedTimeDeal, setLimitedTimeDeal] = useState(false);
+
+  useEffect(() => {
+    setStarRating(4 + Math.random());
+    setLimitedTimeDeal(Math.random() > 0.5);
+  }, []);
+
+  const handleAddToCart = (item) => {
+    addToCart(
+      item.id,
+      {
+        id:item.id,
+        name: item.name,
+        quantity: 1,
+        sku: `Sku-${item.id}`,
+        brand: item.brand,
+        price: item.price,
+        image: item.image,
+      },1);
+    successToast("Item Added to Cart !!");
+  };
+
+
   return (
     <>
     <div className="product-card2">
@@ -15,7 +43,7 @@ function ProductCard({product}) {
         <div className="product-rating">
           <span>
           <StarRatings
-            rating={4 + Math.random()}
+            rating={starRating}
             starRatedColor="orange"
             starDimension="20px"
             starSpacing="5px"
@@ -26,13 +54,19 @@ function ProductCard({product}) {
           <br/>
           <strong>Brand:</strong> {product.brand}
         </div>
-        {Math.random() >0.5 && <span className="product-deal">Limited time deal</span>}
+        {limitedTimeDeal && <span className="product-deal">Limited time deal</span>}
         <div className="product-price">
           <span className="current-price">₹{product.price}</span>
           <span className="original-price">M.R.P: ₹{product.discountPrice}</span>
           <span className="discount">{(((product.discountPrice-product.price)/product.discountPrice)*100).toFixed(2)}% off</span>
         </div>
-        <button className="add-to-cart-button">Add to cart</button>
+        <button onClick={(e)=>{
+              handleAddToCart(product)
+              e.target.setAttribute("disabled", "true");
+              setTimeout(()=>{
+                e.target.removeAttribute("disabled");
+              },3000)
+        }} className="add-to-cart-button">Add to cart</button>
       </div>
     </div>    
     </>
