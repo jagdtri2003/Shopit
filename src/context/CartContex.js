a3b3ddb0c68d92909ddabac822d2fbfd501d79e0
Jagdamba Tripathi <jagdtri2003@gmail.com>
2024-09-18 02:24:18 +0530

    fixed setDoc issue

--- src/context/CartContex.js
+++ src/context/CartContex.js
@@ -27,7 +27,7 @@ const CartProvider = ({children}) =>{
             setCart((prevCount) => prevCount + 1);
             return [
               ...prevCartItems,
-              { id, name: item.name, quantity: count, price: item.price, image: item.image, sku:`Sku-${id}`,brand:item.brand,added:item.added},
+              { id, name: item.name, quantity: count, price: item.price, image: item.image, sku:`Sku-${id}`,added:item.added},
             ];
           }
         });
